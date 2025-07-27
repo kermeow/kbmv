6e43de5c906914d5066fabc7e4793a46f30c9d17
jayden <46307433+kermeow@users.noreply.github.com>
2025-07-27 22:52:57 +0100

    default config relative to exe

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,27 @@
 #include <iostream>
 #include <thread>
 
+#ifdef WIN32
+#include <cstdio>
+#include <Windows.h>
+#endif
+
 std::atomic_bool IsRunning(true);
 
 int main(int argc, char **argv) {
     std::cout << "Loading default config..." << std::endl;
     LoadDefaultConfig();
 
-    const char *config_filename = "kbmv.toml";
+    const char *default_config_name = "kbmv.toml";
+    #ifdef WIN32
+        wchar_t exe_path[FILENAME_MAX];
+        GetModuleFileName(nullptr, exe_path, FILENAME_MAX);
+        std::filesystem::path exe_path = std::filesystem::path(std::string(exe_path));
+    #else
+        std::filesystem::path exe_path = std::filesystem::read_symlink("/proc/self/exe");
+    #endif
+    const char *config_filename = exe_path.replace_filename(default_config_name).c_str();
+
     bool write_default_config = true;
     if (argc == 2) {
         config_filename = argv[1];
@@ -23,7 +37,7 @@ int main(int argc, char **argv) {
     bool config_exists = std::filesystem::exists(config_filename);
     if (!config_exists) {
         if (write_default_config) {
-            std::cout << "Writing default config..." << std::endl;
+            std::cout << "Writing default config... " << config_filename << std::endl;
             WriteDefaultConfig(config_filename);
         }
     } else {
